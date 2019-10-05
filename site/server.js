c51fb56fbd24d93f498c8ba2cc75b9e19c2e6f2c
Назар Назар <nazarhorban99@gmail.com>
2019-10-05 13:46:31 +0300

    add more logic

--- site/server.js
+++ site/server.js
@@ -1,15 +1,38 @@
 
 
 const fs=require('fs');
-const session = require('express-session')
+const session = require('session')
 const express=require('express')
-var market=express();
-market.set('view engine','ejs');
-//const html = fs.createReadStream(__dirname + "/index.html",'utf-8');
+const jwt = require('jsonwebtoken')
+const bcrypt =require('bcrypt')
+const market=express();
 
+market.set('view engine','ejs');
+market.use(express.urlencoded({extended:false}))
+//const html = fs.createReadStream(__dirname + "/index.html",'utf-8');
+const users=[]
 market.get('/',(req,res)=>{
     console.log(req.url)
     res.render('index');
 })
 
+
+market.post('/register_s', async (req,res)=>{
+    try {
+        const hasshedpassword= await bcrypt.hash(req.body.password,10)
+        users.push({
+            id:Date.now().toString(),
+            login:req.body.name,
+            email: req.body.email,
+            password:hasshedpassword
+        })
+        res.redirect('/')
+        console.log(users)
+    } catch {
+        res.redirect('/register')
+    }
+})
+market.post('/login_s', (req,res)=>{
+    users.find()
+})
 market.listen(3000,()=>console.log("server started"));
